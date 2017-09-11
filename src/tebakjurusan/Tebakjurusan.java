ac80328d0019df27387427f80f16f95138794aff
abdulhp <newmarkbeatbox@gmail>
2017-09-11 13:45:50 +0700

    netbeans project

---
+++ src/tebakjurusan/Tebakjurusan.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tebakjurusan;
+
+/**
+ *
+ * @author Newmark Beatbox
+ */
+public class Tebakjurusan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frametebakjurusan frame = new frametebakjurusan();
+        frame.setVisible(true);
+    }
+    
+}
